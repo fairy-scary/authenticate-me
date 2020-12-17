6bfb15c30a3eb6e0eca7fb12c7b2e3ee34e31f39
fairy-scary <ecoautumn@gmail.com>
2020-12-17 11:37:42 -0600

    finished phase 1

--- backend/app.js
+++ backend/app.js
@@ -13,32 +13,29 @@ const isProduction = environment === 'production';
 const app = express();
 
 app.use(morgan('dev'));
-
 app.use(cookieParser());
 app.use(express.json());
-
 // Security Middleware
 if (!isProduction) {
-  // enable cors only in development
-  app.use(cors());
-}
-// helmet helps set a variety of headers to better secure your app
-app.use(helmet({
-  contentSecurityPolicy: false
-}));
+    // enable cors only in development
+    app.use(cors());
+  }
+  // helmet helps set a variety of headers to better secure your app
+  app.use(helmet({
+    contentSecurityPolicy: false
+  }));
+  
+  // Set the _csrf token and create req.csrfToken method
+  app.use(
+    csurf({
+      cookie: {
+        secure: isProduction,
+        sameSite: isProduction && "Lax",
+        httpOnly: true,
+      },
+    })
+  );
 
-// Set the _csrf token and create req.csrfToken method
-app.use(
-  csurf({
-    cookie: {
-      secure: isProduction,
-      sameSite: isProduction && "Lax",
-      httpOnly: true,
-    },
-  })
-);
-
-app.use(routes); // Connect all the routes
-
+  app.use(routes); // Connect all the routes after they traverse middleware
 
   module.exports = app;
